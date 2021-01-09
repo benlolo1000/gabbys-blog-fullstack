d9fa02395730efccf3616d4c9b54c8a693eb6787
Shane Benlolo <benlolo1000@gmail.com>
2021-01-08 22:23:26 -0500

    reverted back to previous background image method, broken until further notice

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -3,18 +3,19 @@ import { Link } from 'react-router-dom';
 import Rating from './Rating';
 
 const Post = ({ post, index }) => {
-  const imageURL = post.image.substring(9);
+  // const imageURL = post.image.substring(9);
   return (
     <>
       {console.log(post.image)}
       <Link to={`/post/${post._id}`}>
         <div
-          // style={{ backgroundImage: `url(${post.image})` }}
+          style={{ backgroundImage: `url(${post.image})` }}
           className={`postContainer${index}`}
-          style={{
-            backgroundImage: `url(https://safe-basin-58625.herokuapp.com/${post.image})`,
-            // this will need to become localhost:5000 upon deployment I believe...
-          }}
+
+          // style={{
+          //   backgroundImage: `url(http://localhost:3000/uploads/${imageURL})`,
+          //   // this will need to become localhost:5000 upon deployment I believe...
+          // }}
         >
           {/* <img src={post.image} className="postImage" /> */}
           <h3>{post.name}</h3>
