cc1c1e63207d0a0afbf4af1b34707017133d6e7f
Shane Benlolo <benlolo1000@gmail.com>
2021-01-08 22:06:37 -0500

    styles rest of homepage items, adds commented out code for dynamic BG image on post

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -3,13 +3,18 @@ import { Link } from 'react-router-dom';
 import Rating from './Rating';
 
 const Post = ({ post, index }) => {
+  const imageURL = post.image.substring(9);
   return (
     <>
       {console.log(post.image)}
       <Link to={`/post/${post._id}`}>
         <div
-          style={{ backgroundImage: `url(${post.image})` }}
+          // style={{ backgroundImage: `url(${post.image})` }}
           className={`postContainer${index}`}
+          style={{
+            backgroundImage: `url(http://localhost:3000/uploads/${imageURL})`,
+            // this will need to become localhost:5000 upon deployment I believe...
+          }}
         >
           {/* <img src={post.image} className="postImage" /> */}
           <h3>{post.name}</h3>
